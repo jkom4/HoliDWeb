40eef722b8a1a6cd9daefb263db20a55368fbe33
Sandro Verdin <s.verdin@student.helmo.be>
2024-07-23 22:16:24 +0200

    Update to tls port

--- src/services/API.js
+++ src/services/API.js
@@ -4,7 +4,7 @@ import {Navigate, Outlet} from "react-router-dom";
 
 
 
-//const apiUrl = "http://studapps.cg.helmo.be:5010/REST_VERD_WABO"
+//const apiUrl = "https://studapps.cg.helmo.be:5011/REST_VERD_WABO"
 const API_keyCurrentWeather = '82f4768ca7650d896603a5eb038218c6'
 
 /**
